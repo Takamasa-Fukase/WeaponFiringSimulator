cac8f916200261a264640778501d654f77ab38dd
Takamasa-Fukase <ultrafukase@gmail.com>
2024-11-20 15:56:08 +0900

    コード整理とコメント追加

--- WeaponFiringSimulator/SoundPlayer2.swift
+++ WeaponFiringSimulator/SoundPlayer2.swift
@@ -7,20 +7,23 @@
 
 import AudioToolbox
 
-final class SoundPlayer2 {
+struct SoundPlayer2 {
     private var systemSoundIds = [SoundType: SystemSoundID]()
     
     init() {
         SoundType.allCases.forEach { sound in
-            if let soundUrl = Bundle.main.url(forResource: sound.rawValue, withExtension: "mp3") {
-                var soundId: SystemSoundID = 0
-                print("AudioServicesCreateSystemSoundID for \(sound), id: \(soundId)")
-                let result = AudioServicesCreateSystemSoundID(soundUrl as CFURL, &soundId)
-                systemSoundIds[sound] = soundId
-                print("result: \(result), id: \(soundId)")
-            }else {
-                print("読み込み失敗")
+            guard let soundUrl = Bundle.main.url(forResource: sound.rawValue, withExtension: "mp3") else {
+                print("音源\(sound.rawValue)が見つかりません")
+                return
             }
+            // idはinout引数でシステムが自動で割り当てるため、可変なSystemSoundIDのインスタンスを作成して渡す
+            var soundId: SystemSoundID = 0
+            
+            // 音源のURLをSystemSoundIDに割り当て
+            AudioServicesCreateSystemSoundID(soundUrl as CFURL, &soundId)
+            
+            // システムによって割り当てられた（inout引数で変更された）idを辞書に保持
+            systemSoundIds[sound] = soundId
         }
     }
 }
@@ -28,7 +31,7 @@ final class SoundPlayer2 {
 extension SoundPlayer2: SoundPlayerInterface {
     func play(_ sound: SoundType) {
         guard let soundId = systemSoundIds[sound] else {
-            print("playSound soundIdが見つかりません \(sound)")
+            print("\(sound)に対応するSystemSoundIDが見つかりません")
             return
         }
         AudioServicesPlaySystemSound(soundId)
