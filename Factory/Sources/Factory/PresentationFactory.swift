014db163b3f720ab6a3a4616a19ba350b4029911
Takamasa-Fukase <ultrafukase@gmail.com>
2024-11-22 16:40:54 +0900

    VCFactoryをPresentationFactoryに変更し、SoundPlayerの生成メソッドも追加

--- Factory/Sources/Factory/PresentationFactory.swift
+++ Factory/Sources/Factory/PresentationFactory.swift
@@ -1,5 +1,5 @@
 //
-//  ViewControllerFactory.swift
+//  PresentationFactory.swift
 //
 //
 //  Created by ウルトラ深瀬 on 22/11/24.
@@ -8,7 +8,7 @@
 import UIKit
 import Presentation
 
-public final class ViewControllerFactory {
+public final class PresentationFactory {
     public static func createMainViewController() -> ViewController {
         let vc = UIStoryboard(name: "Main", bundle: Bundle.presentation).instantiateInitialViewController() as! ViewController
         let presenter = Presenter(
@@ -17,9 +17,15 @@ public final class ViewControllerFactory {
             weaponActionExecuteUseCase: UseCaseFactory.create()
         )
         vc.inject(
-            soundPlayer: SoundPlayer(),
+            soundPlayer: createSoundPlayer(),
             presenter: presenter
         )
         return vc
+    }
+    
+    // MARK: Private Methods
+    
+    private static func createSoundPlayer() -> SoundPlayerInterface {
+        return SoundPlayer()
     }
 }
